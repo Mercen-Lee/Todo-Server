3289bc69d6314c3cb7647392c7b9672b17f052de
이석호 <mercen@mercen.net>
2022-11-24 11:20:09 +0900

    ADD: Project

---
+++ src/test/java/net/mercen/TodoServer/TodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mercen.TodoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
